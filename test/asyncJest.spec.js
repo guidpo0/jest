fb32cb9f77a53b671c3eed9ee507397572315d5e
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-24 21:06:19 -0300

    Requisito 1 feito

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
-A função answerPhone recebe um parâmetro boleano.
+A função answerPhone recebe um parâmetro booleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
 
 Complete o código abaixo para testar as situações em que
@@ -12,11 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    expect(answerPhone(false)).rejects.toThrow('Infelizmente não podemos atender...');
   });
 });
